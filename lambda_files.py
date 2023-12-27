df9e7c574e74676cd7797279fbb72d70fdcb91fa
Francisco Chiva <kkuetx@gmail.com>
2023-12-27 17:00:20 +0100

    Update lambda_files.py

--- lambda_files.py
+++ lambda_files.py
@@ -70,6 +70,11 @@ def lambda_handler(event, context):
     try:
         files = get_all_files(conn)
         response_body = {
+            'headers': {
+             'Access-Control-Allow-Headers': 'Content-Type',
+             'Access-Control-Allow-Origin': '*',
+             'Access-Control-Allow-Methods': 'OPTIONS,GET'
+            },
             'statusCode': 200,
             'body': json.dumps([file.__dict__ for file in files], default=str)
         }
